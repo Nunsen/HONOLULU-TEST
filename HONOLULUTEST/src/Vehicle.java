381d6afe485b202d4d71174fd51fadfaa826cddf
simof <sifu0001@stud.kea.dk>
2024-04-29 12:50:41 +0200

    LORTEGIT

--- HONOLULUTEST/src/Vehicle.java
+++ HONOLULUTEST/src/Vehicle.java
@@ -1,19 +1,41 @@
 public class Vehicle {
-    String brand;
-    String model;
-    String gear;
-    int horsePower;
-    int fuelTime;
-    int regNr;
-    double odometer;
+    private String brand;
+    private String model;
+    private String gear;
+    private int horsePower;
+    private int fuelTime;
+    private int regNr;
+    private double odometer;
     public Vehicle(String b, String mo, String g, int hp, int ft, int rn, double od ){
-        brand = b;
-        model = mo;
-        gear = g;
-        horsePower = hp;
-        fuelTime = ft;
-        regNr = rn;
-        odometer = od;
+        this.brand = b;
+        this.model = mo;
+        this.gear = g;
+        this.horsePower = hp;
+        this.fuelTime = ft;
+        this.regNr = rn;
+        this.odometer = od;
 
+    }
+    //Fordi vi har private, skal vi lave getter
+    public String getBrand() {
+        return brand;
+    }
+    public String getMode() {
+        return model;
+    }
+    public String getGear () {
+        return gear;
+    }
+    public int getHorsePower() {
+        return horsePower;
+    }
+    public int getFuelTime() {
+        return fuelTime;
+    }
+    public int getRegNr() {
+        return regNr;
+    }
+    public double odometer() {
+        return odometer;
     }
 }
