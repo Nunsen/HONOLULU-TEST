db51d2275351a63c79efb64f63bbf452221c3a92
Nunsen <nuso0001@stud.kea.dk>
2024-05-03 23:06:57 +0200

    lowkey det samme, bare med en udkommenteret alternativ løsning til problematikken, Kan ægte ikke finde ud af hvorfor den ikke printer private kunde informationer ud når man vælger customer, fordi den spørger efter private kundeinformationer i vores chooseCustomer klasse men den fatter ikke den skal printe det.. Du kan se eksempel på når man vælger private så ser den fin ud.

--- HONOLULUTEST/src/contractWriter.java
+++ HONOLULUTEST/src/contractWriter.java
@@ -4,11 +4,12 @@ import java.time.LocalDate;
 
 public class contractWriter {
     public static void writeContract(Customer c, boolean customerIsCompany, Vehicle car, RentingDetails newRenter) throws IOException {
-        //anvender localdate til at bruge den aktuelle dato på hvornår kontrakten oprettes
+        //anvender localdate til at sætte ind i kontrakten, så man ikke manuelt skal skrive datoen ind.
         LocalDate currentDate = LocalDate.now();
 
-                /*Vi vil have systemet til at oprette en fil der hedder Contract + nameDriver + licenseNr på den måde sikrer vi
-                at der bliver oprettet en ny fil for hver kunde, da det vil være et nyt unikt navn for hver gang*/
+        /*Vi vil have systemet til at oprette en fil der hedder Contract + nameDriver + licenseNr på den måde sikrer vi
+        at der bliver oprettet en ny fil for hver kunde, da det vil være et nyt unikt navn for hver gang
+        Vi henter informationen fra vores Customer class.*/
         String fileName = "Contract_" + c.getNameDriver() + "_" + c.getLicenseNr() + ".txt";
 
         //Filen oprettes, der tilføjes append:true da der senere skal tilføjes biltype til samme fil
@@ -21,31 +22,38 @@ public class contractWriter {
         writer.write("This is a new contract created on " + currentDate + "\nSecured and dealt by Honolulu Car Rental APS" + "\n\n");
 
         //Anvender vores toString metode for at printe kundeinformationerne ud i filen (Se customerklasse)
-        writer.write("Customer information:");
+        writer.write("Customer information:\n");
         writer.write(c.toString());
-
-        /*HVIS boolean customerIsCompany er sand(2), og comName (fra CustomerCompany klassen
-        IKKE(!) isEmpty, dvs. den er ikke tom, SÅ skal det printes med over i filen
-        Dette sikrer derfor at der ikke printes company information ud, hvis ikke det er nødvendigt */
 
         if (customerIsCompany && c instanceof CustomerCompany) {
             CustomerCompany customerCompany = (CustomerCompany) c; // Cast c to CustomerCompany
-            writer.write(c.toString());
             writer.write(customerCompany.toString());
         }
-                /*er car IKKE nul dvs. der ER valgt en bil. Så printes iformationerne i toString metoden,
-                som så gennem filewriter append skrives ind i filen*/
-        if (car != null) { // Check if car is not null before using it
-            writer.write("\nChosen Car Information:\n");
-            writer.write(car.toString() + "\n");
-        } else {
-            writer.write("\nNo car information available.\n");
+
+        /* if (customerIsCompany) {
+            writer.write("Customer information:\n");
+            writer.write(c.toString());
+        } else if (!customerIsCompany && c instanceof CustomerCompany) {
+            CustomerCompany customerCompany = (CustomerCompany) c; // Cast c to CustomerCompany
+            writer.write("\n\nCompany Information:\n\n");
+            writer.write(customerCompany.toString());
         }
+        */
+
+        /*er car IKKE nul dvs. der ER valgt en bil. Så printes iformationerne i toString metoden,
+        som så gennem filewriter append skrives ind i filen*/
+                if (car != null) { // Check if car is not null before using it
+                    writer.write("\nChosen Car Information:\n");
+                    writer.write(car.toString() + "\n");
+                } else {
+                    writer.write("\nNo car information available.\n");
+                }
                 /* Sidste del af vores filewriter er udlejeinformation, vi printer informationer
                 via toString metode i RentingDetails klasse, bevidst valg så man senere kan tilføje mere*/
-        writer.write("\nDuration of Rent:");
-        writer.write(newRenter.toString());
+                writer.write("\nDuration of Rent:");
+                writer.write(newRenter.toString());
 
-        writer.close();
-    }
-}
+                writer.close();
+            }
+        }
+
